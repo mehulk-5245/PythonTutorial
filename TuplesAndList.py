8e2b554c1556dcbaf783f31f56810c25d4e5a653
Mehul Y Khandhadiya <53016215+mehulk-5245@users.noreply.github.com>
2020-10-01 13:43:31 +0530

    Update TuplesAndList.py

--- TuplesAndList.py
+++ TuplesAndList.py
@@ -8,12 +8,18 @@ def main():
     Ages=[44,33,45,33,54]
     Ages.append(100)
     Ages.insert(0,33)
+    Ages.pop()
+    Ages.pop(2)
+    Ages.remove(44)
     print(Ages)
+    print(sum(Ages))
+    print(Ages[:2])
     #Tuples
     Ages=[44,33,45,33,54]
     Ages.append(100)
     Ages.insert(0,33)
     print(Ages)
+    
 
 
 
